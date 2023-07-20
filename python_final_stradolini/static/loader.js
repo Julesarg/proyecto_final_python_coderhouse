eef1a28206a5db38cb9867d4e3654b44b1ab2c1c
Julian Stradolini <julianstradolini@gmail.com>
2023-07-20 01:43:16 -0300

    minor fix to previous commit and begin of footer

--- python_final_stradolini/static/loader.js
+++ python_final_stradolini/static/loader.js
@@ -1,8 +1,6 @@
 let variable;
 function executeLoader() {
   variable = setTimeout(showPage,4000);
-  let boot = "run"
-  console.log(boot)
 }
 function showPage() {
   document.getElementById("loader").style.display = "none";
