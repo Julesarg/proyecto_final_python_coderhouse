989aa4d279e4717ee509c26a468dea248a9f9d07
Julian Stradolini <julianstradolini@gmail.com>
2023-07-29 20:29:48 -0300

    confirm add and delete item ok

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,14 +4,19 @@ from gallery import views
 urlpatterns = [
     path('', views.gallery_home, name='gallery_home'),
     path('search_success/', views.searchByMat, name = 'search_success'),
+    path('add_success/', views.add_success, name='add_success'),
+    path('modify_success/', views.modify_success, name='modify_success'),
     path('search_fail', views.search_fail, name='search_fail'),
     path('add_item', views.add_item, name='add_item'),
+
     path('paintings/list', views.PaintingList.as_view(), name='paintings'),
     path('sculptures/list', views.SculptureList.as_view(), name='sculptures'),
     path('wood_and_metal/list', views.WoodMetalList.as_view(), name='wood_and_metal'),
     path('digital_assets/list', views.NftList.as_view(), name='digital_assets'),
     path('other_craftings/list', views.OtherList.as_view(), name='other_craftings'),
+
     path(r'^(?P<pk>\d+)$', views.ItemDetail.as_view(), name='detail_item'),
-    path(r'product/edit/(?P<pk>\d+)$', views.ItemUpdate.as_view(), name='edit'),
-    path(r'product/delete/(?P<pk>\d+)$', views.ItemDelete.as_view(), name='delete')    
+
+    path(r'edit/(?P<pk>\d+)$', views.ItemUpdate.as_view(), name='edit'),
+    path(r'delete/(?P<pk>\d+)$', views.ItemDelete.as_view(), name='delete')    
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from gallery.forms import *
 from gallery.models import *
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
+
+## normal views##
 def gallery_home(request):
     return render(request, 'gallery_home.html')
 def search_fail(request):
     return render(request, 'gallery/search_fail.html')
+def add_success(request):
+    return render(request, 'gallery/add_success.html')
+def modify_success(request):
+    return render(request, 'gallery/modify_success.html')
+
+
+##ADD ITEM OK##
 def add_item(request):
     if request.method == "POST":
         myForm = ItemForm(request.POST, request.FILES)
@@ -16,12 +26,13 @@ def add_item(request):
             inf = myForm.cleaned_data
             item = Item(item_name = inf['item_name'], item_created = inf['item_created'],item_price = inf['item_price'],item_description = inf['item_description'],item_genre = inf['item_genre'], item_material = inf['item_material'], item_image = inf['item_image'], item_stock = inf['item_stock'] )
             item.save()
-        return render(request, 'gallery_home.html', {'myText':f'{myForm.as_p()}'})
+        return render(request, 'add_success.html', {'myText':f'{myForm.as_p()}', 'item':item})
     else:
         myForm = ItemForm()
     return render(request, 'add_item.html', {'myForm': myForm})
 
-##listado de items general por genero##
+
+##LIST GENRES OK##
 class PaintingList(ListView):
     model = Item
     template_name = 'paintings.html'
@@ -38,22 +49,23 @@ class OtherList(ListView):
     model = Item
     template_name = 'other_craftings.html'
 
+##LIST DETAIL VIEW OK##
 class ItemDetail(DetailView):
     model = Item
     template_name = 'detail_item.html'
 
+##delete view##
 class ItemDelete(DeleteView):
     model = Item
-    success_url ="/gallery/"
-     
+    success_url ="/gallery/"     
     template_name = "item_confirm_delete.html"
 
+## update view##
 class ItemUpdate(UpdateView):
     model = Item
     success_url = '/gallery/'
     template_name = 'item_form.html'
     fields = ['item_name','item_created', 'item_price', 'item_description', 'item_image', 'item_stock', 'item_genre', 'item_material']
-
 
 ##back button#############################
 def on_backbutton_clicked(self, widget):
