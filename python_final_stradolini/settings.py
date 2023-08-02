8c2307b42faa2ae1ec9e8cdf52f0955b542e5e36
Julian Stradolini <julianstradolini@gmail.com>
2023-08-02 19:38:18 -0300

    modified settings

--- python_final_stradolini/settings.py
+++ python_final_stradolini/settings.py
@@ -137,8 +137,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 # Default primary key field type
