ca1ef422f8c06f9b1645941fd4cb070f3d691803
Julian Stradolini <julianstradolini@gmail.com>
2023-08-02 21:02:02 -0300

    correction on media root

--- python_final_stradolini/settings.py
+++ python_final_stradolini/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 # STATIC_ROOT = BASE_DIR / '/static/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
